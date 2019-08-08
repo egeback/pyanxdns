462d578d5615a57a902c637ce7c9c0ffb4f65923
Marky Egebäck <marky@egeback.se>
2019-08-08 08:25:33 +0200

    add update

--- pyanxdns/cli.py
+++ pyanxdns/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from functools import partial
 from .core import API
 from .core import api_url_base
 from .helpers import format_json
@@ -9,11 +10,68 @@ def add_base_arguments(parser, key, domain):
     parser.add_argument("-k", "--apikey", required=False, help="API key used in request header", default=key)
     parser.add_argument("-d", "--domain", required=False, help="Domain name", default=domain)
     parser.add_argument("-v", "--verbose", required=False, help="Verbose", default=False, action='store_true')
-    parser.add_argument('-n', '--name', required=False, help='Name parameter', default=None)
 
 def add_extended_arguments(parser, key, domain):
-    parser.add_argument('-t', '--ttl', required=False, help='Time to live parameter', default=None)
-    parser.add_argument('-x', '--text', required=False, help='Txt parameter', default=None)
+    parser.add_argument('-t', '--ttl', required=False, help='Time to live parameter', default=3600, type=int)
+
+def add_get_parsers(subparsers, key, domain):
+    parser_get = subparsers.add_parser('get', help='Get records', aliases=['g'])
+    parser_get.add_argument('-n', '--name', required=False, help='Name parameter', default=None)
+
+    add_base_arguments(parser_get, key, domain)
+
+def add_add_parsers(subparsers, key, domain):
+    parser_add = subparsers.add_parser('add', help='Add record', aliases=['a'])
+    parser_record_type = parser_add.add_subparsers(title='Record type', help='Record type to add', dest="type")
+    
+    parser_txt = parser_record_type.add_parser('txt', help='Txt record', aliases=["TXT"])
+    parser_a = parser_record_type.add_parser('a', help='A record', aliases=["A"])
+    parser_cname = parser_record_type.add_parser('cname', help='CNAME record', aliases=["A"])
+
+    parser_txt.add_argument("name", help='Record name')
+    parser_txt.add_argument("txt", help='Record txt')
+    parser_a.add_argument("name", help='Record name')
+    parser_a.add_argument("address", help='Record address')
+    parser_cname.add_argument("name", help='Record name')
+    parser_cname.add_argument("address", help='Record address')
+
+    parsers = [parser_add, parser_txt, parser_a, parser_cname]
+
+    for p in parsers:
+        add_base_arguments(p, key, domain)
+        add_extended_arguments(p, key, domain)
+
+def add_update_parsers(subparsers, key, domain):
+    parser_update = subparsers.add_parser('update', help='Update record', aliases=['u'])
+    parser_update.add_argument('-l', '--line', required=False, help='line parameter', default=None)
+
+    parser_record_type = parser_update.add_subparsers(title='Record type', help='Record type to add', dest="type")
+    
+    parser_txt = parser_record_type.add_parser('txt', help='Txt record', aliases=["TXT"])
+    parser_a = parser_record_type.add_parser('a', help='A record', aliases=["A"])
+    parser_cname = parser_record_type.add_parser('cname', help='CNAME record', aliases=["CNAME"])
+
+    parser_txt.add_argument("-n", "--name", help='Record name')
+    parser_txt.add_argument("-r", "--line", help='Record line')
+    parser_txt.add_argument("txt", help='Record txt')
+
+    parser_a.add_argument("-r", "--line", help='Record line')
+    parser_a.add_argument("-n", "--name", help='Record name')
+    parser_a.add_argument("address", help='Record address')
+    parser_cname.add_argument("-r", "--line", help='Record line')
+    parser_cname.add_argument("-n", "--name", help='Record name')
+    parser_cname.add_argument("address", help='Record address')
+
+    parsers = [parser_update, parser_txt, parser_a, parser_cname]
+
+    for p in parsers:
+        add_base_arguments(p, key, domain)
+
+def add_delete_parsers(subparsers, key, domain):
+    parser_delete = subparsers.add_parser('delete', help='Delete record', aliases=['d', 'del'])
+    parser_delete.add_argument('-r', '--line', required=False, help='Line parameter', default=None)
+    parser_delete.add_argument('-n', '--name', required=False, help='Name parameter', default=None)
+
 
 class CLI:
     def start(self, *param):
@@ -22,20 +80,12 @@ class CLI:
         domain = os.environ['ANXDNS_DOMAIN'] if 'ANXDNS_DOMAIN' in os.environ else None
         
         subparsers = parser.add_subparsers(title='Actions', help='Action to perform', dest="action")
-        parser_get = subparsers.add_parser('get', help='Get records', aliases=['g'])
-        
-        parser_add = subparsers.add_parser('add', help='Add record', aliases=['a'])
-        
-        parser_update = subparsers.add_parser('update', help='Update record', aliases=['u'])
-        parser_update.add_argument('-l', '--line', required=False, help='line parameter', default=None)
+        add_base_arguments(parser, key, domain)
 
-        parser_delete = subparsers.add_parser('delete', help='Delete record', aliases=['d', 'del'])
-        parser_delete.add_argument('-l', '--line', required=False, help='line parameter', default=None)
-
-        for p in [parser, parser_add, parser_get, parser_update, parser_delete]:
-            add_base_arguments(p, key, domain)
-            if p is not parser_get:
-                add_extended_arguments(p, key, domain)
+        add_get_parsers(subparsers, key, domain)
+        add_add_parsers(subparsers, key, domain)
+        add_update_parsers(subparsers, key, domain)
+        add_delete_parsers(subparsers, key, domain)
 
         args = parser.parse_args()
 
@@ -52,7 +102,7 @@ class CLI:
                 parser.print_help()
                 return 1
             
-            if key is None or domain is None:
+            if api_key is None or api_key is "" or domain is None:
                 print("anxdnsapi: error: No apikey or domain provided")
                 parser.print_help()
                 return 1
@@ -88,9 +138,34 @@ class CLI:
                 print(format_json(all_records))
 
     def add(self, api, args):
+        ttl = args.ttl if 'ttl' in args else 3600
+        record_type = args.type
+        
+        method = {
+            "txt": partial(api.add_txt_record, args.name, args.txt if 'txt' in args else "", ttl=ttl),
+            "cname": partial(api.add_cname_record, args.name, args.address if 'address' in args else "", ttl=ttl),
+            "a": partial(api.add_a_record, args.name, args.address if 'address' in args else "", ttl=ttl)
+        }
+        
         print("add")
+        method[record_type.lower()]()
     
     def update(self, api, args):
+        ttl = args.ttl if 'ttl' in args else 3600
+        record_type = args.type
+        txt = args.name if 'txt' in args else None
+        address = args.name if 'address' in args else None
+        name = args.name if 'name' in args else None
+        line = args.line if 'line' in args else None
+        
+        method = {
+            "txt": partial(api.update_txt_record, txt, name=name, ttl=ttl, line=line),
+            "cname": partial(api.update_cname_record, address, name=name, line=line, ttl=ttl),
+            "a": partial(api.update_a_record, address, name=name, line=line, ttl=ttl)
+        }
+
+        method[record_type.lower()]()
+
         print("update")
     
     def delete(self, api, args):
--- pyanxdns/core.py
+++ pyanxdns/core.py
@@ -33,7 +33,6 @@ class Method(Enum):
 
     def __str__(self):
         return self.value
-    
 
 
 class API:
@@ -41,18 +40,25 @@ class API:
         self.domain = domain
         self.apikey = apikey
 
-    def _communicate(self, body=None, method=Method.GET, headers={}, data=None):
+    def _communicate(self, method=Method.GET, headers={}, data_json=None, query_string=""):
         if not 'apikey' in headers:
             headers["apikey"] = self.apikey
         
         if not 'Content-Type' in headers:
             headers["Content-Type"] = "application/json"
         
-        api_url = api_url_base +  '?domain={}'.format(self.domain)
+        api_url = api_url_base + query_string
+
+
+        data = json.dumps(data_json)
 
         response = None
 
-        response = requests.request(str(method), api_url, headers=headers, json=data, allow_redirects=True)
+        try:
+            print(api_url)
+            response = requests.request(str(method), api_url, headers=headers, json=data, allow_redirects=True)
+        except Exception as e:
+            raise APIError(str(e))
 
         if response is None:
             raise APIError("Did not get a response from API")
@@ -71,7 +77,7 @@ class API:
             raise APIError(response.content)
 
     def get_all(self):
-        response = self._communicate(self)
+        response = self._communicate(self, query_string='?domain={}'.format(self.domain))
         return response['dnsRecords']
 
     def parse_by_name(self, all_records, name):
@@ -85,20 +91,34 @@ class API:
                 records.append(record)
         
         return records
+    
+    def parse_by_line(self, all_records, line):
+        for record in all_records:
+            if record['line'] == line:
+                return record
+
+        return None
 
     def get_by_name(self, name):
         all_records = self.get_all()
         
         return self.parse_by_name(all_records, name)
 
-    def _create_json_data(self, name, adress="", type=RecordType.TEXT, ttl=3600, txtdata=None, line=None):
+    def get_by_line(self, line):
+        all_records = self.get_all()
+        
+        return self.parse_by_line(all_records, line)
+
+    def _create_json_data(self, type, name, address=None, ttl=3600, txtdata=None, line=None):
         data = {
             "domain": self.domain,
+            "type": str(type),
             "name": name,
-            "address": adress,
-            "type": type,
             "ttl": ttl
-        }
+        }   
+
+        if address is not None:
+            data["address"] = address
 
         if txtdata is not None:
              data["txtdata"] = txtdata
@@ -106,34 +126,78 @@ class API:
         if line is not None:
             data["line"] = str(line)
 
-        return json.dumps(data)
+        return data
 
 
     def add_txt_record(self, name, txtdata, ttl=3600):
-        data = self._create_json_data(name, ttl=ttl, txtdata=txtdata)
+        data = self._create_json_data(RecordType.TEXT, name, address="", ttl=ttl, txtdata=txtdata)
+        print(data)
 
+        self._communicate(method=Method.POST, data_json=data)
 
-    
     def add_a_record(self, name, address, ttl=3600):
-        pass
-    
-    def add_cname(self, name, cname, ttl=3600):
-        pass
-    
-    def update_txt_record(self, name, ttl=3600):
-        pass
-    
-    def update_a_record(self, name, address, ttl=3600):
-        pass
-    
-    def update_cname(self, name, cname, ttl=3600):
-        pass
+        data = self._create_json_data(RecordType.A, name, address=address, ttl=ttl)
 
-    def delete_row(self, row):
-        pass
+        self._communicate(method=Method.POST, data_json=data)
+    
+    def add_cname_record(self, name, address, ttl=3600):
+        data = self._create_json_data(RecordType.CNAME, name, address=address, ttl=ttl)
+
+        self._communicate(method=Method.POST, data_json=data)
+
+    def verify_or_get_line(self, line, name, type):
+        if line is not None:
+            record = self.get_by_line(line)
+            print(record)
+            if record is None:
+                raise APIError("0 records with provided line number.")
+        elif name is not None:
+            records = self.get_by_name(name)
+            if len(records) == 0:
+                raise APIError("0 records with that name.")
+            elif len(records) > 1:
+                raise APIError(">1 record with that name. Specify line instead of name.")
+            print(records)
+            line = records[0]["line"]
+        else:
+            raise APIError("Line or name needs to be provided")
+        
+        if record is not str(type):
+            raise APIError("Record is not a {}.".format(type))
+
+        return line
+    
+    def update_txt_record(self, txt, name=None, ttl=3600, line=None):
+        # Find line
+        line = self.verify_or_get_line(line, name, RecordType.TEXT)
+
+        data = self._create_json_data(RecordType.TEXT, name, txtdata=txt, ttl=ttl, line=line)
+
+        print(data)
+    
+    def update_a_record(self, address, name=None, ttl=3600, line=None):
+        # Find line
+        line = self.verify_or_get_line(line, name, RecordType.A)
+
+        data = self._create_json_data(RecordType.A, name=name, address=address, ttl=ttl, line=line)
+
+        print(data)
+    
+    def update_cname_record(self, address, name=None, ttl=3600, line=None):
+        # Find line
+        line = self.verify_or_get_line(line, name, RecordType.CNAME)
+        
+        data = self._create_json_data(RecordType.CNAME, name=name, address=address, ttl=ttl, line=line)
+
+        print(data)
+
+    def delete_line(self, line):
+        # Find line
+        line = self.verify_or_get_line(line, None)
     
     def delete_by_name(self, name):
-        pass
+        # Find line
+        line = self.verify_or_get_line(None, name)
 
 if __name__ == "__main__":
     pass
