86ff005021741d36abbac6bd4efc65c1fb022854
Marky Egebäck <marky@egeback.se>
2019-08-10 21:11:34 +0200

    Added LICENCE

--- setup.py
+++ setup.py
@@ -4,9 +4,20 @@ from distutils.core import setup
 
 setup(name='pyanxdns',
       version='0.1',
+      license='MIT'
       description='Python client to communicate with ANX DNS API',
       author='Marky Egebäck',
       author_email='marky@egeback.se',
       url='https://github.com/egeback/pyanxdns',
       packages=['pyanxdns'],
+      install_requires=[],
+      classifiers=(
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Topic :: Utilities",
+        )
      )
