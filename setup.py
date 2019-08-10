b195d31e12ae1bd466f20ea035c3fe1100ec94d7
Marky Egebäck <marky@egeback.se>
2019-08-10 21:37:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='pyanxdns',
-      version='0.1',
+      version='0.2',
       license='MIT',
       description='Python client to communicate with ANX DNS API',
       author='Marky Egebäck',
       author_email='marky@egeback.se',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/egeback/pyanxdns',
       packages=['pyanxdns'],
       install_requires=['request'],
